94c666522b897d99d90f043ed30455e63a15d94d
korkmazkadir <korkmazkadir@gmail.com>
2015-11-29 18:40:11 +0200

    Maven project created with SPRING INITIALIZR

---
+++ src/main/java/com/kadirkorkmaz/ams/AthleteMonitoringSystemApplication.java
@@ -0,0 +1,12 @@
+package com.kadirkorkmaz.ams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthleteMonitoringSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AthleteMonitoringSystemApplication.class, args);
+    }
+}
